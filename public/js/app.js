0c3997ed87901f63500a7abfe66a8e908e8b4098
darnocer <darnocer@gmail.com>
2020-07-08 19:56:49 -0600

    initial html/css/images

---
+++ public/js/app.js
@@ -0,0 +1,19 @@
+// query selectors
+const cartBtn = $(".cart-btn");
+const closeCartBtn = $(".close-cart");
+const clearCartBtn = $(".clear-cart-btn");
+const cartDOM = $(".cart");
+const cartOverlay = $(".cart-overlay");
+const cartItems = $(".cart-items");
+const cartTotal = $(".cart-total");
+const cartContent = $(".cart-content");
+const productsDOM = $(".products-center");
+
+// main cart
+let cart = [];
+
+// getting the product
+class Products {}
+
+// display products
+class UI {}
